514a2ef8508ff1c1cc36aa665553d8505057cce8
sqd <dragonazd@gmail.com>
2015-11-19 12:28:23 +0800

    add config model

---
+++ backend/config/views.py
@@ -0,0 +1,10 @@
+# from django.shortcuts import render
+
+# Create your views here.
+
+def ifShowRequirementTextbox(request):
+    pass
+
+def getHomepageButtonType(request):
+    # Return "ticket" or "info" in plain text
+    pass
