9898bf4cf3a2c4cc405ce7eaac81f934bbf73303
black_binary <black_binary@qq.com>
2015-11-19 03:19:58 -0800

    Basic function

--- backend/ticket/views.py
+++ backend/ticket/views.py
@@ -1,24 +1,56 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 from ticket.models import TicketApplication
+from django.utils.html import *
 
 # Create your views here.
 
+__ERROR="error"
+__ILLEGAL="illegal"
+__FAILURE="failure"
+__SUCCESS="success"
+
+def fliterPost(request,name):
+    try:
+        return escape(request.POST[name])
+    except:
+        raise IOError(__ERROR)
+
+def fliterCode(request):
+    try:
+        return escape(request.session["code"])
+    except:
+        raise IOError(__ERROR)
+
 def validateInput(data):
     # Validate the input, return True if is valid, otherwise False
-    pass
+    if len(data.name)>4 or len(data.schoolID)>4:
+        raise IOError(__ILLEGAL)
 
 def extractInput(request):
     # Extract inputs from request
-    pass
+    data=TicketApplication()
+    data.name=fliterPost(request,"name")
+    data.organization.grade=fliterPost(request,"grade")
+    data.organization.no=fliterPost(request,"class")
+    data.schoolID=fliterPost(request,"schoolID")
+    data.societyID=fliterPost(request,"ID")
+    data.requirement=fliterPost(request,"requirement")
+    if fliterPost(request,"captcha")!=fliterCode(request):
+        raise IOError(__FAILURE)
+    return data
 
 def insertApplication(request):
     # Argument passed by POST (containing sensitive info)
     # Return success state in plain text ("success"/"illegal"/"failure"/"error")
-    data = extractInput(request)
     try:
-        if not validateInput(data):
-            return HttpResponse("illegal")
-        pass
+        data = extractInput(request)
+        validateInput(data)
+    except IOError,e:
+        return HttpResponse(e)
+    try:
+        data.save()
     except:
-        return HttpResponse("error")
+        return HttpResponse(__ERROR)
+    return HttpResponse(__SUCCESS)
+
