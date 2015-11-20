23f1a836cd11e8c98b899a43b412d9b80bee453b
black_binary <black_binary@qq.com>
2015-11-20 21:29:46 +0800

    Deleted org, fixed validateIDCode()

--- backend/ticket/models.py
+++ backend/ticket/models.py
@@ -2,13 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Organization(models.Model):
-    grade = models.PositiveIntegerField()
-    no = models.PositiveIntegerField()
-
 class TicketApplication(models.Model):
+    grade = models.PositiveIntegerField(default = 0)
+    no = models.PositiveIntegerField(default = 0)
     name = models.CharField(max_length = 4)
-    organization = models.ForeignKey(Organization)
     schoolID = models.CharField(max_length = 6)
     societyID = models.CharField(max_length = 18)
     requirement = models.PositiveIntegerField(default = 1)
--- backend/ticket/views.py
+++ backend/ticket/views.py
@@ -3,23 +3,26 @@ from django.http import HttpResponse
 from ticket.models import *
 from django.utils.html import *
 
-# Create your views here.
-
 __ERROR="error"
 __ILLEGAL="illegal"
 __FAILURE="failure"
 __SUCCESS="success"
 
-def validateIDCode(l):  
-    return True
+def validateIDCode(ID):  
+    l=list(ID)
     if len(l)!=18:
         return False
-    sum = 0  
-    for ii,n in enumerate(l):  
-        i = 18-ii
-        weight = 2**(i-1) % 11  
-        sum = (sum + int(n)*weight) % 11  
+    for i,it in enumerate(l):
+        if it=='x' or it=='X':
+            l[i]=10
+        else:
+            l[i]=int(it)
+    sum=0  
+    for i,it in enumerate(l):  
+        weight = 2**(17-i) % 11  
+        sum = (sum + int(it)*weight) % 11  
     return sum==1  
+
 
 def fliterPost(request,name):
     try:
@@ -33,31 +36,21 @@ def fliterCode(request):
     except:
         raise IOError(__ERROR)
 
-def extractInput(request):
-    name=fliterPost(request,"name")
-    grade=int(fliterPost(request,"grade"))
-    no=int(fliterPost(request,"class"))
-    schoolID=fliterPost(request,"schoolID")
-    societyID=fliterPost(request,"ID")
-    requirement=int(fliterPost(request,"requirement"))
-
-    if len(name)>4 or len(schoolID)>4 or requirement<=0 or not validateIDCode(societyID):
-        raise IOError(__ILLEGAL)
-
-    organization=Organization(grade=grade,no=no)
-    data=TicketApplication(name=name,organization=organization,schoolID=schoolID,societyID=societyID,requirement=requirement)
-    try:
-        organization.save()
-        data.save()
-    except:
-        raise IOError(__ERROR)
 
 
 def insertApplication(request):
-    # Argument passed by POST (containing sensitive info)
-    # Return success state in plain text ("success"/"illegal"/"failure"/"error")
     try:
-        extractInput(request)
+        ID=fliterPost(request,"ID")
+        #if not validateIDCode(ID) or fliterCode(request)!=fliterPost(request,"captcha"):
+        #    raise IOError(__ILLEGAL)
+        TicketApplication.objects.create(
+        name=fliterPost(request,"name"),
+        grade=int(fliterPost(request,"grade")),
+        no=int(fliterPost(request,"class")),
+        schoolID=fliterPost(request,"schoolID"),
+        societyID=fliterPost(request,"ID"),
+        requirement=int(fliterPost(request,"requirement")),
+        )
     except IOError,e:
         return HttpResponse(e)
     return HttpResponse(__SUCCESS)
