e8ecb47e1eb3ab6bd046308d434f495da0e3d854
sqd <dragonazd@gmail.com>
2015-11-20 22:04:41 +0800

    add several APIs

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -2,17 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class Organization(models.Model):
-    grade = models.PositiveIntegerField()
-    no = models.PositiveIntegerField()
-
 class ShopApplication(models.Model):
     owner = models.CharField(max_length = 4)
+    ownerGrade = models.PositiveIntegerField()
+    ownerClass = models.PositiveIntegerField()
     ownerContact = models.CharField(max_length = 64)
     shopName = models.CharField(max_length = 32)
     ownerType = models.PositiveIntegerField(default = 0)
-    ownerOrganization = models.ForeignKey(Organization)
     electricity = models.BooleanField(default = False)
     food = models.BooleanField(default = False)
     nonFood = models.BooleanField(default = False)
     privilegeKey = models.CharField(max_length = 8)
+    timestamp = models.DateTimeField()
