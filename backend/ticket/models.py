32fa1bb2c947547720ae4202f1607ea4445d6573
blackbinary <black_binary@qq.com>
2015-11-22 12:29:56 +0800

    Strange bug (TicketApplication.grade and no needs default value) fixed

--- backend/ticket/models.py
+++ backend/ticket/models.py
@@ -4,11 +4,11 @@ from datetime import datetime
 # Create your models here.
 
 class TicketApplication(models.Model):
-    grade = models.CharField(max_length=1)
-    no = models.CharField(max_length = 2)
+    grade = models.CharField(max_length=1,default="0")
+    no = models.CharField(max_length = 2,default="0")
     name = models.CharField(max_length = 4)
     schoolID = models.CharField(max_length = 6)
     societyID = models.CharField(max_length = 18)
-    requirement = models.PositiveIntegerField()
+    requirement = models.CharField(max_length=32)
     timestamp = models.DateTimeField(default = datetime(1,1,1))
 
