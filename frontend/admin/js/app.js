74b4f7ca38315a40ff4ca9fe94106ecf80841183
singsong <alskdjfhg@foxmail.com>
2015-11-25 09:33:34 +0800

    Created Layouts

---
+++ frontend/admin/js/app.js
@@ -0,0 +1,7 @@
+(function() {
+    var loader_el = document.getElementById("loader");
+    loader_el.style.display = "none";
+
+    var container_el = document.getElementById("container");
+    container_el.style.display = "";
+})();
